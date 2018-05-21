43867acdd2144ca2bd4301b1aed52ac763850fe1
Jonathan <jon.mly@gmail.com>
2018-05-21 13:42:36 +0200

    reset head

--- srcs/main.c
+++ srcs/main.c
@@ -5,6 +5,7 @@ int		main(int ac, char **av)
 	t_env		*env;
 
 	env = init_env();
+	ft_putendl("Begin redering");
 	calculate_scene(env);
 	mlx_key_hook(env->win_ptr, handle_key_event, (void*)env);
 	mlx_loop(env->mlx_ptr);
